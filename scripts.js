6f1160ad444822e71a400b63961b5a1a57d1cc77
AlexandruC97 <63367791+AlexandruC97@users.noreply.github.com>
2021-03-03 13:57:50 +0200

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function reply_click(clicked_id) {
-    var x = Math.floor(Math.random() * 3);
-    if (x == clicked_id) {
-        alert("Well done, mate!");
-    }else {
-        alert("Neah!!! Try again!");
-    }
-}
-
